5e10f7add575eaad4d1b74992769f5f6fc38989c
JimTheMan <mrdotjim@gmail.com>
2018-04-18 14:45:24 -0400

    basic binance

---
+++ .creds_EXAMPLE.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "key",
+  secret: "secret"
+};
--- src/jims-binance-functions.js
+++ src/jims-binance-functions.js
@@ -8,9 +8,21 @@
 
 const axios = require('axios');
 const errorResponse = {'error': 'Please pass query parameter "character" with a value 0 - 10.'};
+const creds = require('./../.creds');
+const binance = require('node-binance-api');
+
+
 class JimsBinanceFunctions {
 
-  constructor() {
+  constructor(symbols) {
+
+    binance.options({
+      APIKEY: '<key>',
+      APISECRET: '<secret>',
+      useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+      test: true // If you want to use sandbox mode where orders are simulated
+    });
+
     this.axios = axios;
   }
 
@@ -36,6 +48,8 @@ class JimsBinanceFunctions {
   hello() {
     return new Promise((resolve, reject) => {
 
+      console.log('credskey is ' + creds.key);
+
       // const intNumber = parseInt(number);
       //
       // if (isNaN(intNumber)) {
@@ -58,10 +72,71 @@ class JimsBinanceFunctions {
       //   });
 
 
-      resolve({'some stuff': 'thingies'});
+      // resolve({'some stuff': 'thingies'});
+
+      resolve(this.getPrice('BNBBTC'));
 
     })
   }
+
+
+  /**
+   *
+   * takes a ticker and returns a promise that resolves to the data around that ticker.
+   *
+   * @param ticker
+   *
+   * example of return value: { ETHBTC: '0.07003500' }
+   */
+  getPrice(tickerName) {
+    return new Promise((resolve, reject) => {
+      binance.prices(ticker, (error, ticker) => {
+        console.log("Price of crypto (" + tickerName + "): " + ticker[tickerName]);
+        resolve(ticker[tickerName]);
+      })
+    })
+  };
+
+  /**
+   *
+   * takes a ticker and returns a promise that resolves to the data around that ticker.
+   *
+   * @param ticker
+   *
+   * example of return value:
+   *
+   * {
+      "symbol": "BNBBTC",
+      "bidPrice": "4.00000000",
+      "bidQty": "431.00000000",
+      "askPrice": "4.00000200",
+      "askQty": "9.00000000"
+     }
+   */
+  getBidAskPrices(tickerName) {
+    return new Promise((resolve, reject) => {
+      binance.bookTickers(ticker, (error, ticker) => {
+        console.log("Bid / Ask prices of crypto (" + tickerName + "): " + ticker[tickerName]);
+        resolve(ticker[tickerName]);
+      })
+    })
+  };
+
+
+  // getMarketDepth(ticker)
+
+  // placeLimitOrder(ticker)
+
+  // placeStopLossOrder(ticker)
+
+  // cancelOpenOrders(s)(ticker)
+
+  // cancelOpenOrders(s)(ticker)
+
+  // get24hrChangeDifference(ticker)
+
+  // get "kline / candelstick" data(ticker)
+
 
 }
 
