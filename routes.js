5da7d84c71911403222a775720c1d62d8b585cc3
JimTheMan <mrdotjim@gmail.com>
2018-04-24 01:09:06 -0400

    removed post

--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
-const emptyRouteHandler = require('./src/routes/empty-route');
 const express = require('express');
 const routes = express();
 
+const emptyRouteHandler = require('./src/routes/empty-route');
+
 routes.get('/', emptyRouteHandler);
 
+
+// Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = routes;
